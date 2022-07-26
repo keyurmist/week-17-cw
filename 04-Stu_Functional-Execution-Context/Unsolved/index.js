977d5a0bab415283d60a156640d9e8ec157738d0
KeyurMistry <keyurmist@gmail.com>
2022-07-26 21:07:22 +0100

    completed ex4

---
+++ 04-Stu_Functional-Execution-Context/Unsolved/index.js
@@ -0,0 +1,14 @@
+// returns the average value of an array of numbers
+function avg(array) {
+  let total = 0;
+  function sum() {
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+  }
+  sum(array);
+
+  return total / array.length;
+}
+
+module.exports = avg;
