c6bd86b725dbf90e483f8102cd8afd221283c0a5
KeyurMistry <keyurmist@gmail.com>
2022-07-26 20:10:26 +0100

    added comments to ex2

--- 02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
+++ 02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
@@ -1,25 +1,26 @@
 // 1) Where is carNoise stored?
-//
-const carNoise = 'Honk';
+// GEC global execution context
+const carNoise = "Honk";
 // 2) Where is goFast stored?
-// 
-const goFast = speed => {
+// GEC Global execution context
+const goFast = (speed) => {
   // 4) When is speed assigned a value? Where is this value stored?
-  // 
-  
+  // When the function goFast is called then speed is assigned a value.
+  // It is stored in the functions on memory, its' execution context.
   // 5) Where is makeNoise stored?
-  //
-  const makeNoise = sound => {
+  // in the execution context of goFast
+  const makeNoise = (sound) => {
     console.log(`My speed is at ${speed}, time to ${sound}`);
-  }
+  };
 
   // 6) What happens in the following statement?
-  // 
+  // taking the global value of carNoise and placed on the stack to create
+  // a new function execution context
   makeNoise(carNoise);
-}
+};
 
 // 3) What happens in the following statement?
-// 
-if(confirm("Do you want to go fast?")) {
+// once you click ok, it creates a FEC for goFast
+if (confirm("Do you want to go fast?")) {
   goFast(80);
 }
