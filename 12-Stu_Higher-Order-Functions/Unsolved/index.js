a38fce94894a9a6735438c4b8e8927b8a8bfa248
KeyurMistry <keyurmist@gmail.com>
2022-07-28 19:29:51 +0100

    completed findAverage function for ex12

--- 12-Stu_Higher-Order-Functions/Unsolved/index.js
+++ 12-Stu_Higher-Order-Functions/Unsolved/index.js
@@ -2,10 +2,13 @@ const grades = [56, 78, 99, 85];
 
 // TODO: Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
-    
+  if (index === array.length - 1) {
+    return (accumulator + currentValue) / array.length;
+  }
+  return accumulator + currentValue;
 }
 
 // TODO: Pass the 'findAverage' into the reduce method.
-let gradeAverage = grades.reduce();
+let gradeAverage = grades.reduce(findAverage);
 
 console.log(gradeAverage);
