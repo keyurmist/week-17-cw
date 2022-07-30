20756f446b83a7345adc19cf26af0141c718e655
KeyurMistry <keyurmist@gmail.com>
2022-07-30 10:30:08 +0100

    added comments for exerecise 20

--- 20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
+++ 20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
@@ -1,20 +1,24 @@
-const buttons = document.getElementsByTagName('button');
+const buttons = document.getElementsByTagName("button");
 
 const clickHandler = function () {
-// TODO: Add a comment describing the purpose of this variable.
+  // TODO: Add a comment describing the purpose of this variable.
+  // this variable will be part of the click handler function
   let count = 0;
 
-  // TODO: Add a comment describing what is happening. 
+  // TODO: Add a comment describing what is happening.
+  // this returns an inner function
   return function () {
-  
-   // TODO: Add a comment describing how this variable is being scoped. 
+    // TODO: Add a comment describing how this variable is being scoped.
+    // increases the local scope count by 1
     count++;
-   // TODO: Add a comment describing how we are using the 'count' variable.
+    // TODO: Add a comment describing how we are using the 'count' variable.
+    // this displays the count variable on the button
     this.textContent = `Clicks: ${count}`;
   };
 };
 
 // TODO: Add a comment describing the purpose of this loop.
+// sets up a click handler for each button
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', clickHandler());
+  buttons[i].addEventListener("click", clickHandler());
 }
